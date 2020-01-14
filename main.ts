9f9291658cbb9a2d7863726d3785b77189af462d
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-01-15 05:54:05 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/*
+  split
+*/
+//% color=#006464 weight=20 icon="\uf1b9" block="split"
+namespace split {
+/**
+ * TODO:文字列をカンマで分解する
+ * @param text 文字列。, eg: "ABCD,abcd"
+ */
+//% blockId="文字列を分解" block="文字列を分解 %text"
+//% weight=100 blockGap=8
+    function split(text: string):string[] {
+        let work: string[] = []
+        for (let index = 0; index < text.length ; index++) {
+            if (text.charAt(index) == ",") {
+                work.push("")
+            } else {
+                work[work.length] = work[work.length] + text.charAt(index)
+            }
+        }
+        return work;
+    }
+}
