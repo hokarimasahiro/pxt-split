e872ffea3fee7cd923061331da35da9aa57ca33e
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-01-18 11:45:32 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -3,19 +3,36 @@
 */
 //% color=#006464 weight=20 icon="\uf1b9" block="split"
 namespace split {
-/**
- * TODO:文字列をカンマで分解する
- * @param text 文字列。, eg: "ABCD,abcd"
- */
-//% blockId="文字列を分解" block="文字列を分解 %text"
-//% weight=100 blockGap=8
-    export function split(text: string):string[] {
-        let work: string[] = []
-        for (let index = 0; index < text.length ; index++) {
+    /**
+     * TODO:文字列をカンマで分解する
+     * @param text 文字列。, eg: "ABCD,abcd"
+     */
+    //% blockId="文字列を分解" block="文字列を分解 %text"
+    //% weight=100 blockGap=8
+    export function split(text: string): string[] {
+        let work: string[] = [""]
+        for (let index = 0; index < text.length; index++) {
             if (text.charAt(index) == ",") {
                 work.push("")
             } else {
-                work[work.length] = work[work.length] + text.charAt(index)
+                work[work.length - 1] = work[work.length - 1] + text.charAt(index)
+            }
+        }
+        return work;
+    }
+    /**
+     * TODO:数値をカンマで分解する
+     * @param text 文字列。, eg: "1234,56,10"
+     */
+    //% blockId="数値を分解" block="数値を分解 %text"
+    //% weight=100 blockGap=8
+    export function splitNum(text: string): number[] {
+        let work: number[] = [0]
+        for (let index = 0; index < text.length; index++) {
+            if (text.charAt(index) == ",") {
+                work.push(0)
+            } else {
+                work[work.length - 1] = work[work.length - 1] * 10 + parseInt(text.charAt(index))
             }
         }
         return work;
